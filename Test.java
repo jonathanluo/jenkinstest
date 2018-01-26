be68cc373d43c8191ca41823cfc228da12ed78cb
moonwave <moonwave.systems@gmail.com>
2018-01-25 23:03:59 -0800

    update

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] argvs) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello " + i);
+        for (int j = 0; j < 10; i++) {
+            System.out.println("Hello World " + j);
         }
     }
 }
